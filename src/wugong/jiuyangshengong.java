db5f5a33e50ae9c374f94740d74d62f11b063475
lcy-zxf <185859824@qq.com>
2019-12-06 19:07:14 +0800

    111

--- src/wugong/jiuyangshengong.java
+++ src/wugong/jiuyangshengong.java
@@ -4,7 +4,8 @@ public class jiuyangshengong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("1111111");
+		System.out.println("22222");
 	}
 
 }
